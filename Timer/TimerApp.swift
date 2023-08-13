e8c216cb9a5161391e76e62bc460ad21ac6b74b6
Taha Chaudhry <taha.chaudhry@icloud.com>
2023-08-13 10:36:21 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Taha Chaudhry on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
